aa3fa5592ef9923f58cc3e0e5ad9c0e6705bdb61
Antonio Espinoza <aespinozareyes9@gmail.com>
2025-08-28 15:58:45 -0400

    add lifecycle methods

--- src/test/java/com/antonio/junitdemo/DemoUtilsTest.java
+++ src/test/java/com/antonio/junitdemo/DemoUtilsTest.java
@@ -1,14 +1,38 @@
 package com.antonio.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach(){
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach(){
+        System.out.println("@AfterEach executes after the execution of each test method");
+        System.out.println();
+    }
+
+    @BeforeAll
+    static void setupBeforeEachClass(){
+        System.out.println("@BeforeAll executes before all the test methods");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll(){
+        System.out.println("@AfterAll executes after all the test methods");
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testEqualsAndNotEquals");
         assertEquals(2, demoUtils.add(1, 1), "1+1 must be 2");
         assertNotEquals(4, demoUtils.add(1, 2), "1+2 must not be 4");
 
@@ -16,7 +40,7 @@ class DemoUtilsTest {
 
     @Test
     void testCheckNullWithNull() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testCheckNullWithNull");
         assertNull(demoUtils.checkNull(null), "Should return null when input is null");
     }
 
